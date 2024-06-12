a0c30049ace8a3d399acba7d53d4ca7341cf2eac
BrunoSupriano <brunosupriano@hotmail.com>
2024-06-12 20:02:38 -0300

    Projeto Base

---
+++ src/main/java/nature/calculadoraco2/CalculadoraCo2Application.java
@@ -0,0 +1,13 @@
+package nature.calculadoraco2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraCo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculadoraCo2Application.class, args);
+    }
+
+}
