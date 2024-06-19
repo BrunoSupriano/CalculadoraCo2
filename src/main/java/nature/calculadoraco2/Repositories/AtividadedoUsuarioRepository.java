68d7e246afa08cd51dded9ff546196d2ed16f4fc
Julia Meller <juliameller@live.com>
2024-06-19 15:57:56 -0300

    ajustes estrutura

--- src/main/java/nature/calculadoraco2/Repositories/AtividadedoUsuarioRepository.java
+++ src/main/java/nature/calculadoraco2/Repositories/AtividadedoUsuarioRepository.java
@@ -1,6 +1,5 @@
 package nature.calculadoraco2.Repositories;
 
-import nature.calculadoraco2.Model.AtividadedoUsuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
